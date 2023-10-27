a78f4eaf00ff314eee2d7f250ac840593f9d444c
PauloTesi <53013901+PauloTesi@users.noreply.github.com>
2023-10-27 16:59:01 -0300

    Add files via upload

---
+++ src/domain/product/factory/product.factory.ts
@@ -0,0 +1,14 @@
+import Product from "../entity/product";
+import ProductInterface from "../entity/product.interface";
+import { v4 as uuid } from "uuid";
+import ProductB from "../entity/product-b";
+
+export default class ProductFactory {
+  public static create(
+    name: string,
+    price: number
+  ) {
+    return new Product(uuid(), name, price);
+  }
+}
+
